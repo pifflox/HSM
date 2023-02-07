a54eeafc3ff4cd190d6698eca6dc90afcaeb90d4
Daiyan Rafi <daiyanrafi1@gmail.com>
2023-02-07 16:53:40 +0600

    login 0602 1653

--- controllers/login.js
+++ controllers/login.js
@@ -1,71 +1,77 @@
-var express = require("express");
+var express = require ('express');
+var mysql =require('mysql');
+var session = require ('express-session');
 var router = express.Router();
-var bodyParser = require("body-parser");
-var db = require("../models/db_controller");
-var mysql = require("mysql");
-var session = require("express-session");
-var sweetalert = require("sweetalert2");
-const { check, validationResult } = require("express-validator");
+var bodyParser = require('body-parser');
+var db = require.main.require ('./models/db_controller');
+var  sweetalert = require('sweetalert2');
+const { check, validationResult } = require('express-validator');
+
 
 
 var con = mysql.createConnection({
-  host: "localhost",
-  password: "",
-  database: "hmsystem",
+
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'hmsystem'
 });
 
-router.use(
-  session({
-    secrect: 'somevalue',
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+router.use(session({
 
-router.use(bodyParser.urlencoded({ extended: true }));
+    secret: 'secret',
+    resave : true ,
+    saveUninitialized : true 
+
+}));
+
+
+router.use(bodyParser.urlencoded({extended : true}));
 router.use(bodyParser.json());
 
-router.post(
-  "/",
-  [
-    check("username").notEmpty().withMessage("Username is required"),
-    check("password").notEmpty().withMessage("Password is required"),
-  ],
-  function (request, response) {
-    const errors = validationResult(request);
 
+router.post('/',[
+    check('username').notEmpty().withMessage("Username is reequired"),
+    check('password').notEmpty().withMessage("Password is reequired")
+    
+], function(request , response){
+    const errors = validationResult(request);
     if (!errors.isEmpty()) {
-      return response.status(422).json({ errors: errors.array() });
-    }
+        return response.status(422).json({ errors: errors.array() });
+      }
+
     var username = request.body.username;
     var password = request.body.password;
 
-    if (username && password) {
-      con.query(
-        'select * from users where username = ? and password = ?',
-        [username, password],
-        function (error, results, fields) {
-          if (results.length > 0) {
-            request.session.loggedin = true;
-            request.session.username = username;
-            response.cookie("username", username);
-            var status = results[0].email_status;
-            if (status == "not_verified") {
-              response.send("verify");
-            } else {
-              sweetalert.fire("Login Successful");
-              response.redirect("/home");
+    if (username && password){
+        con.query('select * from users where username = ? and password = ?' , [username, password], function(error , results , fields){
+            if (results.length > 0){
+                
+                request.session.loggedin = true ; 
+                request.session.username = username;
+                response.cookie('username' , username);
+                var status = results[0].email_status;
+                if (status=="not_verified" ){
+                    response.send("please verify your email");
+                }
+                else{
+                    sweetalert.fire('logged In!');
+                    response.redirect('/home');
+                }
+               
+            }else{
+                response.send('Incorrect username / password');
             }
-        } else {
-          response.send("Incorrect Username and/or Password!");
-        }
+            response.end();
+        });
+
+    }else{
+        response.send('please enter user name and password');
         response.end();
-          })
-        
-  } else {
-    response.send("Please enter Username and Password!");
-    response.end();
-  }
-})
+    }
+
+});
 
 module.exports = router;
+
+///path: models/db_controller.js
